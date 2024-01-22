2d459dcb074fb2638805b976b6399918e40f41e5
Joowon Yang <jywleah1101@yonsei.ac.kr>
2024-01-22 23:40:33 +0900

    background_loop
    배경 스크롤링

--- soccer-run/Assets/Scripts/Background_Change.cs
+++ soccer-run/Assets/Scripts/Background_Change.cs
@@ -6,8 +6,10 @@ public class Background_Change : MonoBehaviour
 {
     public GameObject backgroundPrefab; //배경 프리팹
     public int numberOfBackgrounds = 10; //생성할 배경의 수
-    public float backgroundWidth = 10f; //생성할 배경의 너비
-
+    public float backgroundDepth = 10f; //생성할 배경의 너비
+    public Transform playerTransform; // 배경의 이동에 참고할 player의 위치값
+    private GameObject[] backgrounds;
+    private float backmostBackgroundZ;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +20,42 @@ public class Background_Change : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerTransform.position.z > backmostBackgroundZ)
+        {
+            Debug.Log(backmostBackgroundZ);
+            MoveBack();
+        }
     }
 
     void SpawnBackgrounds()
     {
+        backgrounds = new GameObject[numberOfBackgrounds];
+
         for(int i = 0; i < numberOfBackgrounds; i++)
         {
             // 배경 프리팹의 인스턴스를 생성하고 위치를 설정
-            GameObject background = Instantiate(backgroundPrefab, new Vector3(i * backgroundWidth, 0, 0), Quaternion.identity);
-            background.transform.SetParent(transform); // 생성된 배경을 현재 오브젝트의 자식으로 설정 (선택 사항)
+            GameObject background = Instantiate(backgroundPrefab, new Vector3(0, 0, i * backgroundDepth), Quaternion.identity);
+            //background.transform.SetParent(transform); // 생성된 배경을 현재 오브젝트의 자식으로 설정 (선택 사항)
+            backgrounds[i] = background;
         }
+        backmostBackgroundZ = backgrounds[2].transform.position.z;
+        
     }
+
+    void MoveBack()
+    {
+        GameObject backmostBackground = backgrounds[0];
+        backmostBackground.transform.position = new Vector3(0, 0, backmostBackground.transform.position.z + numberOfBackgrounds * backgroundDepth);
+
+        // Rearrange the array
+        for (int i = 0; i < numberOfBackgrounds - 1; i++)
+        {
+            backgrounds[i] = backgrounds[i + 1];
+        }
+        backgrounds[numberOfBackgrounds - 1] = backmostBackground;
+        backmostBackgroundZ = backgrounds[2].transform.position.z;
+
+    }
+
 }
+
